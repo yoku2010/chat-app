57a8bc979e1bd9b102e199f43a9b9b2cf8444d76
yoku2010 <yoku2010@gmail.com>
2017-01-22 00:11:32 +0530

    Broadcast events

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on("connect", function() {
     console.log("connected to server");
-
-    socket.emit("createMessage", {
-        from: "Yogesh",
-        text: "Hey......"
-    });
 });
 socket.on("disconnect", function() {
     console.log("disconnected from server");
--- server/server.js
+++ server/server.js
@@ -15,13 +15,24 @@ io.on("connection", (socket) => {
     console.log("New User connected");
 
     socket.emit("newMessage", {
-        from: "john",
-        text: "Hey, How are you?",
-        createdAt: 1223
+        from: "Admin",
+        text: "Welcome to the chat app",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit("newMessage", {
+        from: "Admin",
+        text: "New user joined!",
+        createdAt: new Date().getTime()
     });
 
     socket.on("createMessage", (message) => {
         console.log("createMessage", message);
+        io.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on("disconnect", () => {
