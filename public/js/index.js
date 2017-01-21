3cb0f9c18e32d8eeb1087d3afba85cd6e6775b59
yoku2010 <yoku2010@gmail.com>
2017-01-22 02:08:13 +0530

    Add form for message and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,25 @@
-var socket = io();
-socket.on("connect", function() {
-    console.log("connected to server");
-});
-socket.on("disconnect", function() {
-    console.log("disconnected from server");
-});
-socket.on("newMessage", function(message) {
-    console.log("newMessage", message);
-})
+(function ($) {
+    var socket = io(), $ol = $("#messages");
+    socket.on("connect", function () {
+        console.log("connected to server");
+    });
+    socket.on("disconnect", function () {
+        console.log("disconnected from server");
+    });
+    socket.on("newMessage", function (message) {
+        console.log("newMessage", message);
+        var $li = $("<li></li>").text(`${message.from}: ${message.text}`);
+        $li.appendTo($ol);
+    });
+
+    var $form = $("#message_form").on("submit", function (e) {
+        e.preventDefault();
+        socket.emit("createMessage", {
+            from: "Yogesh",
+            text: $("#id_message").val()
+        }, function (data) {
+            console.log("got it", data);
+        });
+        $form[0].reset();
+    });
+})(jQuery);
--- server/server.js
+++ server/server.js
@@ -16,13 +16,14 @@ app.use(express.static(publicPath));
 io.on("connection", (socket) => {
     console.log("New User connected");
 
-    socket.emit("newMessage", generateMessage("Admin", "Welcom to the chat app"));
+    socket.emit("newMessage", generateMessage("Admin", "Welcome to the chat app"));
 
     socket.broadcast.emit("newMessage", generateMessage("Admin", "New user joined!"));
 
-    socket.on("createMessage", (message) => {
+    socket.on("createMessage", (message, callback) => {
         console.log("createMessage", message);
         io.emit("newMessage", generateMessage(message.from, message.text));
+        callback("this is from the server");
     });
 
     socket.on("disconnect", () => {
